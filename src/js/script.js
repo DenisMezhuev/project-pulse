5d16bc53fefbf271a90eda21551507f278d364c6
Denis Ermakov <alexmezhuev@yandex.by>
2024-01-20 12:43:36 +0300

    create file

---
+++ src/js/script.js
@@ -0,0 +1,136 @@
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    infinite: true,
+    speed: 1200,
+    slidesToShow: 1,
+
+    prevArrow:
+      '<button type="button" class="slick-prew"><img src="img/slide/left-solid.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="img/slide/right-solid.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  $(".catalog-item__link").each(function (i) {
+    $(this).on("click", function (e) {
+      e.preventDefault();
+      $(".catalog-item__content")
+        .eq(i)
+        .toggleClass("catalog-item__content_active");
+      $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+    });
+  });
+
+  $(".catalog-item__back").each(function (i) {
+    $(this).on("click", function (e) {
+      e.preventDefault();
+      $(".catalog-item__content")
+        .eq(i)
+        .toggleClass("catalog-item__content_active");
+      $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+    });
+  });
+
+  // Modal
+
+  $("[ data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
+
+  function valideForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+        },
+        phone: {
+          required: "Пожалуйста, введите свой номер телефон",
+        },
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты",
+        },
+      },
+    });
+  }
+
+  valideForms("#consultation-form");
+  valideForms("#consultation form");
+  valideForms("#order form");
+
+  $("form").button_submit(function (e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn("slow");
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+});
